81a16e37555ef2fc62acae4a264d654dc7a78655
v.medvedev <6fSkDfxQ5cYTmTN9muNg>
2022-05-25 11:01:27 +0500

    #1.5 (Hello World!)

--- app/src/main/java/com/free/courses/MainActivity.kt
+++ app/src/main/java/com/free/courses/MainActivity.kt
@@ -2,10 +2,17 @@ package com.free.courses
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext, "Alimuhammad", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(applicationContext, "Alimuhammad", Toast.LENGTH_SHORT).show()
     }
 }
